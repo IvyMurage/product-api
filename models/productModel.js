b2853300dd0afcedbc491bca79bfc81a6e212317
Murage-Ivy <ivymurage2000@gmail.com>
2024-08-30 08:54:33 +0200

    create a product schema

---
+++ models/productModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+
+const Schema = new Schema()
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+})
+
+const Product = mongoose.model("Product", productSchema)
+
+module.exports = Product
